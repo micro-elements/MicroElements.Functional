060dc60dc82306c90427184270dc8f254b32156e
Alexey Petryashev <petriashev@gmail.com>
2019-10-19 15:28:37 +0300

    fix build

--- build.sh
+++ build.sh
@@ -75,7 +75,8 @@ echo "restoring MicroElements.DevOps"
 dotnet restore $CAKE_PROPS_PATH --packages $TOOLS_DIR --source "$NUGET_URL"
 echo "restoring global tools"
 #dotnet tool restore
-dotnet tool install --global --tool-path $TOOLS_DIR Cake.Tool --version 0.35.0
+#dotnet tool install --global --tool-path $TOOLS_DIR Cake.Tool --version 0.35.0
+dotnet tool install --global Cake.Tool --version 0.35.0
 
 # Start Cake
 echo "Running build script..."
