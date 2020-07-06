9abc65f23201fa4eef645263633fd928f9bd2258
Alexey Petryashev <petriashev@gmail.com>
2020-07-06 16:21:02 +0300

    devops updated

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@
 
 echo "Starting build.sh"
 
-DEVOPS_VERSION=1.9.1
+DEVOPS_VERSION=1.11.0
 NUGET_URL=https://api.nuget.org/v3/index.json
+NUGET_BETA_URL=https://www.myget.org/F/micro-elements/api/v3/index.json
 
 # Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -17,7 +18,7 @@ TOOLS_DIR=$SCRIPT_DIR/tools
 SCRIPT=$TOOLS_DIR/microelements.devops/$DEVOPS_VERSION/scripts/main.cake
 
 CAKE_PROPS_PATH=$TOOLS_DIR/cake.props
-CAKE_VERSION="0.35.0"
+CAKE_VERSION="0.38.0"
 CAKE_ARGUMENTS=()
 
 # Parse arguments.
@@ -40,6 +41,7 @@ echo "SCRIPT_DIR: $SCRIPT_DIR"
 echo "SCRIPT: $SCRIPT"
 echo "TOOLS_DIR: $TOOLS_DIR"
 echo "NUGET_URL: $NUGET_URL"
+echo "NUGET_BETA_URL: $NUGET_BETA_URL"
 echo "CAKE_PROPS_PATH: $CAKE_PROPS_PATH"
 echo "CAKE_ARGUMENTS: ${CAKE_ARGUMENTS[*]}"
 
@@ -59,7 +61,7 @@ then
     cat > "$CAKE_PROPS_PATH" <<EOL
 <Project Sdk="Microsoft.NET.Sdk">
 <PropertyGroup>
-  <TargetFramework>netstandard2.0</TargetFramework>
+  <TargetFramework>netcoreapp3.0</TargetFramework>
 </PropertyGroup>
 <ItemGroup>
   <PackageReference Include="MicroElements.DevOps" Version="$DEVOPS_VERSION" />
@@ -71,15 +73,11 @@ EOL
 fi
 
 # Restore Cake and Packages
-echo "restoring MicroElements.DevOps"
-dotnet restore $CAKE_PROPS_PATH --packages $TOOLS_DIR --source "$NUGET_URL"
-echo "restoring global tools"
-#dotnet tool restore
-CAKE_PATH="$TOOLS_DIR/dotnet-cake"
-dotnet tool install --tool-path $TOOLS_DIR Cake.Tool --version 0.35.0
+dotnet restore $CAKE_PROPS_PATH --packages $TOOLS_DIR --source "$NUGET_URL" --source "$NUGET_BETA_URL"
+dotnet tool restore
 
 # Start Cake
 echo "Running build script..."
-CMD="$CAKE_PATH $SCRIPT ${CAKE_ARGUMENTS[@]}"
+CMD="dotnet cake $SCRIPT ${CAKE_ARGUMENTS[@]}"
 echo $CMD
 exec $CMD
