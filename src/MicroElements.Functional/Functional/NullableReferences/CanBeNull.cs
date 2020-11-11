1f9e9bc96da0f16342bd03b36e78677d2c7d382c
Alexey Petryashev <petriashev@gmail.com>
2020-11-11 14:51:05 +0300

    CanBeNull and CanNotBeNull added

---
+++ src/MicroElements.Functional/Functional/NullableReferences/CanBeNull.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) MicroElements. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace MicroElements.Functional
+{
+    /// <summary>
+    /// Represents value that can be null in terms of nullable-references.
+    /// See: https://docs.microsoft.com/en-us/dotnet/csharp/nullable-references.
+    /// </summary>
+    /// <typeparam name="T">Value type.</typeparam>
+    public readonly struct CanBeNull<T>
+    {
+        /// <summary>
+        /// Gets value that can be null in terms of nullable-references.
+        /// </summary>
+        [MaybeNull]
+        [AllowNull]
+        public T Value { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CanBeNull{T}"/> struct.
+        /// </summary>
+        /// <param name="value">Value that can be null.</param>
+        public CanBeNull([AllowNull] T value) => Value = value;
+
+        /// <summary>
+        /// Implicit conversion to the base value type.
+        /// </summary>
+        /// <param name="canBeNull"><see cref="CanBeNull{T}"/> value.</param>
+        [return: MaybeNull]
+        public static implicit operator T(in CanBeNull<T> canBeNull) => canBeNull.Value;
+
+        /// <summary>
+        /// Implicit conversion to <see cref="CanBeNull{T}"/>.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        public static implicit operator CanBeNull<T>([AllowNull] T value) => new CanBeNull<T>(value);
+    }
+}
---
+++ src/MicroElements.Functional/Functional/NullableReferences/CanNotBeNull.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) MicroElements. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MicroElements.Functional
+{
+    /// <summary>
+    /// Represents value that can not be null in terms of nullable-references.
+    /// See: https://docs.microsoft.com/en-us/dotnet/csharp/nullable-references.
+    /// Note: CanNotBeNull can not be created with null value so you can use it safely in your code.
+    /// </summary>
+    /// <typeparam name="T">Value type.</typeparam>
+    public readonly struct CanNotBeNull<T>
+    {
+        private readonly bool _isInitialized;
+        private readonly T _value;
+
+        /// <summary>
+        /// Gets value that can not be null in terms of nullable-references.
+        /// </summary>
+        [NotNull]
+        [DisallowNull]
+        public T Value
+        {
+            get
+            {
+                if (!_isInitialized)
+                    throw new InvalidOperationException("default(CanNotBeNull) can not be used uninitialized.");
+                return _value!;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CanBeNull{T}"/> struct.
+        /// </summary>
+        /// <param name="value">Value that can not be null.</param>
+        /// <exception cref="ArgumentNullException">value is null.</exception>
+        public CanNotBeNull([DisallowNull] T value)
+        {
+            // AssertArgumentNotNull is from MicroElements.Functional
+            _value = value.AssertArgumentNotNull(nameof(value));
+            _isInitialized = true;
+        }
+
+        /// <summary>
+        /// Implicit conversion to the base value type.
+        /// </summary>
+        /// <param name="canBeNull"><see cref="CanBeNull{T}"/> value.</param>
+        [return: NotNull]
+        public static implicit operator T(in CanNotBeNull<T> canBeNull) => canBeNull.Value;
+
+        /// <summary>
+        /// Explicit conversion to <see cref="CanNotBeNull{T}"/>.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        public static explicit operator CanNotBeNull<T>([DisallowNull] T value) => new CanNotBeNull<T>(value);
+    }
+}
